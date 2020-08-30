3336068e8a072c649b33e687276c6c0df779f658
Glazzerino <=>
2020-08-29 19:50:40 -0500

    primeros pasos

--- src/sketch.py
+++ src/sketch.py
@@ -1,13 +1,22 @@
 import numpy as np
 
-height = int(input("Largo de la sala: ")) + 2
-width = int(input("Ancho de la sala: ")) + 2
-exit_loc = tuple(map(int,input("Coordenadas de la entradas/salida: ").split(' ')))
-# print(exit_loc[0])
-test = np.full([width,height],'x')
+def algSketch(matrix: np.matrix):
+    
 
-for i in range(1,width-1):
-    for j in range(1,height-1):
+# Visualizacion
+height = int(input("Largo de la sala: ")) 
+width = int(input("Ancho de la sala: ")) 
+exit_loc = tuple(map(int,input("Coordenadas de la entradas/salida: ").split(' '))) # tuple
+
+test = np.full([width+2,height+2],' ')
+
+for i in range(1,width+1):
+    for j in range(1,height+1):
         test[i,j] = 'o'
-
+test[exit_loc] = 'e'
 print(test)
+
+maxquota = height * width
+percentage = 20
+attendees = maxquota * (percentage / 100)
+print(attendees)
