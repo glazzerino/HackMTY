3bd7b8915938698f64bfe62d27662e5c08171a99
Ian De La Garza <46934558+ianyan11@users.noreply.github.com>
2020-08-29 23:59:12 -0500

    ya funciona la pantalla principal

--- html/index.js
+++ html/index.js
@@ -6,8 +6,8 @@ function client_redirect() {
     window.location.replace("./client/client.html")
 }
     
-function userType_redirect(){
-    if (id="customRadio1") {
+function userType_redirect(id){
+    if (id=="customRadio1") {
         owner_redirect()
     } else {
         client_redirect()
