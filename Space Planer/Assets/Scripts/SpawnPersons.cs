d8a9d0fb13875346d54dfe4ff14a0a03c4e34d2a
Richarzone <Richarzonea@gmail.com>
2020-08-30 10:11:59 -0500

    Size

--- Space Planer/Assets/Scripts/SpawnPersons.cs
+++ Space Planer/Assets/Scripts/SpawnPersons.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class SpawnPersons : MonoBehaviour
 {
-    public GameObject personPrefaf;
+    public GameObject personPrefaf, armchairPrefaf;
     private GameObject decoyPrefaf;
     public Vector3 center;
     public Vector3 size;
    
     void Start()
     {
+        SpawnSits();
         //SpawnFood();
         
     }
@@ -19,14 +20,29 @@ public class SpawnPersons : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.G))
         {
-            SpawnFood();
+            SpawnPerson();
         }
     }
 
-    public void SpawnFood()
+    void SetValues(int x, int y, int z)
+    {
+        size.Set(x, y, z);
+    }
+
+    public void SpawnPerson()
     {
         Vector3 position = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
         Instantiate(personPrefaf, position, Quaternion.identity);
+    }
+
+    private void SpawnSits()
+    {
+        for(float i = (-size.x/2) + 1; i <= size.x/2; i++)
+        {
+            Vector3 pos = center + new Vector3(i, 0, 0);
+            //armchairPrefaf.transform.localScale.z = armchairPrefaf.transform.localScale.z * size.z;
+            Instantiate(armchairPrefaf, pos, Quaternion.identity);
+        }
     }
 
     private void OnDrawGizmosSelected()
